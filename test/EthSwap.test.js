e363e384a044a7a93714eb74a79ad913ff045345
sangharsh <sangharsh.wagle@aa.com>
2021-11-18 16:20:25 -0600

    Update EthSwap.test.js

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -1,21 +1,23 @@
+const { assert } = require('chai');
 
 const EthSwap = artifacts.require("EthSwap"); 
+const Token = artifacts.require("Token");
 
 require('chai')
     .use(require('chai-as-promised'))
     .should()
 
 contract ('EthSwap Contract',  () => {
+    let ethSwapContract
+    before( async () => {
+    ethSwapContract = await EthSwap.new()
+    })
   it ('is not null', async () => {
-    const ethSwapContract = await EthSwap.new()
       assert.isNotNull(ethSwapContract)
   })
   it('has valid name',  async () => {
-    const ethSwapContract = await EthSwap.new()
       const contractName =  await ethSwapContract.name()
-      //console.log(contractName)
       assert.equal(contractName, 'Eth Swap Finance')
   })
-  
 })
 
