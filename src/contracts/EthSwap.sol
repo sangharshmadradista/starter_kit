b3d32abc3fb971d95e8e890a5c9dfd611998a5a9
sangharsh <sangharsh.wagle@aa.com>
2021-11-19 17:54:22 -0600

    Feat: Emit after tokens purchased

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -5,7 +5,16 @@ import "./Token.sol";
 contract EthSwap {
 	string public name = "Eth Swap Finance";
 	Token public token;
-	uint public ethExchangeRate = 100; 
+	uint public ethExchangeRate = 100;
+
+	//Event stores all the parameters passed in the blockchain
+	//here we want to log address of account that purchased token, token address, token count and exchange rate
+	event BuyToken(
+		address sender,
+		address token, 
+		uint tokenCount, 
+		uint exchangeRate
+	); 
 
 	constructor (Token _token) public {
 		token = _token;
@@ -18,6 +27,9 @@ contract EthSwap {
 	function buyToken () public payable {
 		uint value = msg.value * ethExchangeRate; 
 		token.transfer(msg.sender, value);
+
+		//emit the event after succesfull transfer
+		emit BuyToken(msg.sender, address(token), value, ethExchangeRate);
 	}
 }
 
