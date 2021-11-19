3919fa0db3b0da94296105f5b1c34959b255228c
sangharsh <sangharsh.wagle@aa.com>
2021-11-19 15:25:09 -0600

    Update: Test cases for buyToken

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -6,8 +6,10 @@ const Token = artifacts.require("Token");
 require('chai')
     .use(require('chai-as-promised'))
     .should()
-
-contract ('EthSwap Contract',  ( [sangharsh , sameer] ) => { // first name will be refrecee to first ganache account and so on
+function tokens(n){
+    return web3.utils.toWei(n, 'ether')
+}
+contract ('EthSwap Contract',  ( [sangharsh , sameer] ) => { // first name will be refrence to first ganache account and so on
     let ethSwapContract, tokenContract
     before( async () => {
     tokenContract = await Token.new()
@@ -21,21 +23,30 @@ contract ('EthSwap Contract',  ( [sangharsh , sameer] ) => { // first name will 
       const contractName =  await ethSwapContract.name()
       assert.equal(contractName, 'Eth Swap Finance')
   })
-  describe ('buyToken()', async () => {
-      let etherCount, dappCount
+  describe ('buyToken(): Purchsed DApp token from EthSwap contract with Ether ', async () => {
+      let etherCount, dappCountPerEth, initalDappCount
      before (async () => {
         etherCount = '1'
-        dappCount = etherCount * 100 // one ether is 100 DAppToken
+        dappCountPerEth = etherCount * 100 // one ether is 100 DAppToken
+        initalDappCount = await tokenContract.balanceOf(ethSwapContract.address)
         /*
             from: msg.sender
             value: msg.value 
          */
         await ethSwapContract.buyToken({from: sameer, value: web3.utils.toWei(etherCount,'ether')})        
      }) 
-    it ("valid amount of token purchased", async () => {
+    it ("valid amount of DApptoken purchased", async () => {
         let currentDAppToken = await tokenContract.balanceOf(sameer)
         currentDAppToken = currentDAppToken.toString()
-        assert.equal(currentDAppToken , web3.utils.toWei(dappCount.toString(),'ether'))
+        assert.equal(currentDAppToken , web3.utils.toWei(dappCountPerEth.toString(),'ether'))
+    })
+    it ("valid amount of DApptoken reduced from EthSWap contract", async () => {
+        let tokenCount = await tokenContract.balanceOf(ethSwapContract.address)
+        assert.equal(tokenCount.toString(), tokens('999900'))
+    })
+    it ("valid amount of Eth with EthSwapContract", async () => {
+        let ethBalance = await web3.eth.getBalance(ethSwapContract.address)
+        assert.equal(ethBalance.toString(), web3.utils.toWei('1', 'ether'))
     })
   })
     
